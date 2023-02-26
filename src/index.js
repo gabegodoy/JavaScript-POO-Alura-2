c64433a87267de9d11c78fbba4ae7d292bb5f7fb
Gabriel Godoy <gabe.profe@gmail.com>
2023-02-26 19:05:34 -0300

    classe 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import { Personagem } from "./modules/personagem.js";
 import { PersonagemView } from "./components/personagem-view.js"; 
 import { Mago } from "./modules/mago.js";
+import { Arqueiro } from "./modules/arqueiro.js";
+import { ArqueiroMago } from "./modules/arqueiro-mago.js";
 
-const personagemPedrinho = new Personagem('Pedrinho', 5, 'Mago');
-const personagemJose = new Personagem('Jose', 2, 'Arqueiro');
 
+const magoAntonio = new Mago('Antonio', 4, 'Fogo', 4, 3);
+const magaJulia = new Mago('Julia', 8, 'Gelo', 7, 10);
+const arqueiroGabriel = new Arqueiro('Gabriel', 55, 20);
+const arqueiroMagoChico = new ArqueiroMago('Chico', 7, 10, 'Ar', 4, 8);
 
-const personagens = [personagemPedrinho, personagemJose];
+const personagens = [magoAntonio, magaJulia, arqueiroGabriel, arqueiroMagoChico];
 
 new PersonagemView(personagens).render();
-
-console.log(new Mago('gelo', 7, 9))
